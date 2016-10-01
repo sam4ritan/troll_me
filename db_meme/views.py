554b477cae6fdf42fbdf47c7601832a4c3917318
sam4ritan <sam4ritan@protonmail.com>
2016-10-01 14:34:12 +0200

    integrated index.html to index-view

--- db_meme/views.py
+++ db_meme/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.template import loader
+from .models import Meme_pic
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello World")
+    template = loader.get_template('db_meme/index.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
