765139fff9573531d8fdbe5cbeaad08219ff1ccc
sam4ritan <sam4ritan@protonmail.com>
2016-10-01 14:16:03 +0200

    Added index view

--- troll_me/urls.py
+++ troll_me/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from db_meme.views import index
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    ##url(r'^admin/', admin.site.urls),
+    url(r'^$', index, name='index' )
 ]
