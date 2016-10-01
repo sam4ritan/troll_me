107a074fd67eb64c5d930a447d3f37c6b0c3e62b
sam4ritan <sam4ritan@protonmail.com>
2016-10-01 12:20:22 +0200

    Changed DB tables and introduced Meme-ID

--- db_meme/models.py
+++ db_meme/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Meme_pic(models.Model):
 	description = models.CharField(max_length=200)
-	ub_date = models.DateTimeField('date uploaded')
+	up_date = models.DateTimeField('date uploaded')
 	format = models.CharField(max_length=4)
-	url = models.CharField(max_length=500)
+	mid = models.AutoField(primary_key=True)
+
+	def __str__(self):
+		return str(self.description) + str(" ") + str(self.up_date) + str(" ") + str(self.format) + str(" ") + str(self.mid)
