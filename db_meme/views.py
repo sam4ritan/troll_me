8a55c090714e33a76395e79148c3c8827d3be60b
zorck97 <zorck97@gmail.com>
2016-10-01 17:15:49 +0200

    fehlende views hinzugefügt

--- db_meme/views.py
+++ db_meme/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 from django.template import loader
 from .models import Meme_pic
 # Create your views here.
 
 def index(request):
+    list = Meme_pic.objects.all()[:1]
     template = loader.get_template('db_meme/index.html')
     context = {
-
+        "items" : list
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'db_meme/item.html', context)
+
+def item(request):
+    if request.method == 'POST':
+        val1 = request.POST.CharField(label="usr")
+
+
+    else:
+        paramVal = request.GET.get('id','')
+
+        if str(paramVal):
+            return HttpResponseRedirect("/")
+
+        itemElement = Meme_pic.objects.get(paramVal)
+        context = {
+            "item" : itemElement
+        }
+        return render(request, 'db_meme/item.html', context)
